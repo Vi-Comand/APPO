1d55ee7f854ba706f4d40eed7cce3ddb953b818b
kravchenko_v_e <kravcenko.vi@live.com>
2021-05-31 10:56:55 +0300

    Добавьте файлы проекта.

---
+++ SISPR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SISPR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SISPR/wwwroot/js/site.js
@@ -0,0 +1,62 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*
+Vue.createApp({
+    data() {
+        return {
+            valueInput: '',
+            needDoList: [],
+            completeList: []
+        };
+    },
+    methods: {
+        load() {
+            alert(1);
+            this.valueInput = this.value;
+            alert(this.value);
+        },
+        handleInput(event) {
+            this.valueInput = event.target.value;
+        },
+        addTask() {
+            if (this.valueInput === '') { return };
+            this.needDoList.push({
+                title: this.valueInput,
+                id: Math.random()
+            });
+            this.valueInput = '';
+        },
+        doCheck(index, type) {
+
+            if (type === 'need') {
+                const completeMask = this.needDoList.splice(index, 1);
+                this.completeList.push(...completeMask);
+            }
+            else {
+                const noCompleteMask = this.completeList.splice(index, 1);
+                this.needDoList.push(...noCompleteMask);
+            }
+        },
+        removeMask(index, type) {
+            const toDoList = type === 'need' ? this.needDoList : this.completeList;
+            toDoList.splice(index, 1);
+        }
+    }
+}
+).mount('#app');
+
+const Demo = {
+    data() {
+        
+           
+            return {
+             1: false
+           
+        }
+    }
+}
+
+Vue.createApp(Demo).mount('.demo')
+*/
