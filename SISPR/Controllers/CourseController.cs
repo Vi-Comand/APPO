a2be0553fbc1170b9498d17b8100db6de07c4c9f
kravchenko_v_e <kravcenko.vi@live.com>
2021-07-21 09:55:22 +0300

    добавил курсы и профиль

---
+++ SISPR/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SISPR.Models;
+using System.Diagnostics;
+
+namespace SISPR.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CourseController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ SISPR/Controllers/LKController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SISPR.Models;
+using System.Diagnostics;
+
+namespace SISPR.Controllers
+{
+    public class LKController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LKController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
